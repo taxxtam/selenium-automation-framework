176527c8f9aa1b26e166868b6b7a511c63694d50
Ashish Yadav <147745449+taxxtam@users.noreply.github.com>
2026-04-15 14:15:28 +0530

    Update LoginPage.java

--- src/test/java/tests/src/test/java/pages/LoginPage.java
+++ src/test/java/tests/src/test/java/pages/LoginPage.java
@@ -1,19 +1,19 @@
-package base;
+package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseTest {
+public class LoginPage {
 
-    protected WebDriver driver;
+    WebDriver driver;
 
-    public void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.google.com");
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void tearDown() {
-        driver.quit();
+    By searchBox = By.name("q");
+
+    public void search(String text) {
+        driver.findElement(searchBox).sendKeys(text);
     }
 }
