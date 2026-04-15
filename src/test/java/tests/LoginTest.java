c84e5b96719c9835814d8fd79099e1067b415e8e
Ashish Yadav <147745449+taxxtam@users.noreply.github.com>
2026-04-15 14:14:07 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,17 @@
-public class LoginTest {
-    public static void main(String[] args) {
-        System.out.println("Automation Test Running");
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void googleSearchTest() {
+
+        LoginPage page = new LoginPage(driver);
+        page.search("Selenium Automation");
+
+        System.out.println("Test Executed Successfully");
     }
 }
